37d444a920932081a4c19f516eb2f95a0be90c9b
Mahalakshmi R <mahalakshmi18168@gmail.com>
2026-07-22 22:15:19 +0530

    Added Arrays - Two Sum solutions

---
+++ Arrays/OptimizeTwoSum.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class OptimizeTwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for(int i=0;i<nums.length;i++){
+
+            int complement = target - nums[i];
+
+            if(map.containsKey(complement)){
+
+                return new int[]{map.get(complement),i};
+
+            }
+
+            map.put(nums[i],i);
+        }
+
+        throw new IllegalArgumentException("No match");
+    }
+
+    public static void main(String[] args){
+
+        OptimizeTwoSum obj = new OptimizeTwoSum();
+
+        int[] nums = {2,7,11,15};
+
+        int target = 9;
+
+        System.out.println(Arrays.toString(obj.twoSum(nums,target)));
+    }
+}
