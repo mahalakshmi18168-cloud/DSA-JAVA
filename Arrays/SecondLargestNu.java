c544f1e25a3271007cb4b3097cddee5ebae750f2
Mahalakshmi R <mahalakshmi18168@gmail.com>
2026-07-30 09:41:43 +0530

    Second Largest number Solution

---
+++ Arrays/SecondLargestNu.java
@@ -0,0 +1,28 @@
+class SecondLargestNu{
+    public static void main(String[] args){
+        int a[]={20,42,6,25,30,88};
+        int max1=0;
+        int max2=0;
+    
+        if(a[0]>max1){
+            max1=a[0];
+            max2=a[1];
+        }
+        else{
+            max2=a[0];
+            max1=a[1];
+        }
+        for(int i=2;i<a.length;i++){
+            if(a[i]>max1){
+                max2=max1;
+                max1=a[i];
+
+            }
+            else if(a[i]>max2){
+                max2=a[i];
+            }
+        }
+        System.out.println(max1);
+        System.out.println(max2);
+    }
+}
