7741664aee8ae99a53e71f21b26baeaaf8ddb7d9
Mahalakshmi R <mahalakshmi18168@gmail.com>
2026-08-07 17:58:37 +0530

    checking array is sorted or not

---
+++ Arrays/SortedArrays.java
@@ -0,0 +1,19 @@
+public class SortedArrays {
+    public static void main(String[] args){
+        int nums[]={1,2,3,4,8,5};
+        for(int i=0;i<nums.length-1;i++){
+            
+                if(nums[i]>nums[i+1]){
+                    System.out.println(false);
+                    return;
+                }
+                   
+                
+            }
+             System.out.println(true);
+            
+        }
+        
+    }
+    
+
